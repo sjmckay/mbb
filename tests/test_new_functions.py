1b821109f2d05e7171a1bc3286cf62bb591ad3cf
Stephen McKay <stephen.mckay@my.wheaton.edu>
2025-09-03 20:07:40 -0500

    edits to posterior

--- tests/test_new_functions.py
+++ tests/test_new_functions.py
@@ -11,16 +11,22 @@ if __name__ == "__main__":
     print('850um flux', m1.eval(850,z=m1.z).value*1000,'mJy')
     print(f'log10 dust mass: {np.log10(m1.dust_mass.value):.2f}')
     print(f'peak wl: {m1.get_peak_wavelength():.2f}')
-    # m1.plot_sed(obs_frame=True)
-    # plt.title('m1')
 
-    m1.fit(phot=([850/(1+m1.z)],[0.0021],[0.00032]),niter=10,to_vary=['N'],restframe=True)
+
+    m1.fit(phot=([450, 850],[0.005, 0.0021],[0.0006,0.00032]),niter=50,to_vary=['N','beta'],restframe=False)
     print('after fit:')
     print('850um flux', m1.eval(850,z=m1.z).value*1000,'mJy')
     print(f'log10 dust mass: {np.log10(m1.dust_mass.value):.2f}')
     print(m1.posterior('N'))
+
+    print(f'ndof, nbands, chi2: {m1.n_dof}, {m1.n_bands}, {m1.chi2}')
     try:
-        print(m1.posterior('beta'))
+        print(m1.posterior('beta',q=50))
+        print(m1.beta)
     except Exception as e:
         print(f'Caught exception {e}')
+
+    m1.plot_sed(obs_frame=True)
+    plt.title('m1')
+    plt.show()
 
