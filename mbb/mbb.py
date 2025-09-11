a50bdf11b1c174eff376145c3d65b0c2e54ecbc8
stephen@berea <sjmckay3@wisc.edu>
2025-09-11 09:05:01 -0500

    use external Pool even if ncores=1

--- mbb/mbb.py
+++ mbb/mbb.py
@@ -148,7 +148,8 @@ class ModifiedBlackbody:
             Wavelengths should be given as rest-frame values.
             nwalkers (int): how many walkers should be used in the MCMC fit. 
             niter (int): how many iterations to run in the fit.
-            ncores (int): how many CPU cores to use in multiprocessing. Pass 1 to not use multiprocessing. Default is number of available CPUs - 2.
+            ncores (int): how many CPU cores to use in multiprocessing. If ``pool`` is not None, this is ignored. Set to 1 to not use multiprocessing. \
+                Default is number of available CPUs - 2.
             stepsize (float): stepsize used to randomize the initial walker values. 
             params (list): list of parameter names, e.g., [``L``, ``beta``, ``T``, ``z``, ``alpha``, ``l0``] to vary in the fit. The rest will be fixed. \
                 ``L`` should generally be included since it represents the normalization of the model. 
@@ -477,11 +478,10 @@ class ModifiedBlackbody:
             ``pos``, ``prob``, and ``state`` are the output of the ``run_mcmc`` function from the ``emcee.EnsembleSampler <https://emcee.readthedocs.io/en/stable/user/sampler/>``_
         """
         close_pool = False
-        if ncores > 1 and pool is None:
-            pool = mp.get_context("spawn").Pool(processes=ncores)
-            close_pool = True
-        elif ncores <= 1:
-            pool = None
+        if pool is None:
+            if ncores > 1:
+                pool = mp.get_context("spawn").Pool(processes=ncores)
+                close_pool = True
         try:
             sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=pool, parameter_names=to_vary, kwargs=fixed)
             print("Running burn-in...")
