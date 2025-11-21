cf08daf241e5c59dd8ca509c11b560b05699f707
stephen@berea <sjmckay3@wisc.edu>
2025-11-21 14:14:54 -0600

    add versioning

--- mbb/__init__.py
+++ mbb/__init__.py
@@ -1,3 +1,10 @@
 from .mbb import ModifiedBlackbody
 from . import mbb_funcs
 __all__ = ["ModifiedBlackbody", "mbb_funcs"]
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("mbb")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
